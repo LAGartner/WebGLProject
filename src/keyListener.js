1cf4cddfcfc2f6f767aac2248992c85ded044db1
Adrian-Garcia <adriangarlop@indexzero.io>
2021-04-24 16:49:47 -0500

    Added keyListener

---
+++ src/keyListener.js
@@ -0,0 +1,10 @@
+document.addEventListener("keydown", onDocumentKeyDown, false);
+
+function onDocumentKeyDown(event) {
+  var keyCode = event.which;
+  if (keyCode == 87) { // W
+  } else if (keyCode == 83) { // S
+  } else if (keyCode == 65) { // A
+  } else if (keyCode == 68) { // D
+  }
+};
